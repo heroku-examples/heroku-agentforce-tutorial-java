9af0657a939a5e7b5ccd192608fa74fffc1ce6a9
Andrew Fawcett <afawcett@salesforce.com>
2024-11-15 00:33:15 -0700

    Clean up

--- src/main/java/com/heroku/examples/agentforce/action/services/AgentActionService.java
+++ src/main/java/com/heroku/examples/agentforce/action/services/AgentActionService.java
@@ -2,8 +2,6 @@ package com.heroku.examples.agentforce.action.services;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
